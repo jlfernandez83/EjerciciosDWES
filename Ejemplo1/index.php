54ced942a265aae169021ee0987b211e9c9f9bde
jlfernandez83 <jlfernandez83@gmail.com>
2026-09-25 13:30:59 +0200

    Ejemplo de cómo usar un archivo de plantillas sin función generadora de marcado.

--- Ejemplo1/index.php
+++ Ejemplo1/index.php
@@ -8,17 +8,8 @@ include(__DIR__.'/src/functions.php');
 
 $coches = getCSVContentInArray(__DIR__.'/data_source/coches.csv');
 
-$output = getCochesMarkupFromData($coches);
+//$outputCoches = getCochesMarkupFromData($coches);
+
+include(__DIR__.'/templates/html.tpl.php');
 
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php echo $output; ?>
-</body>
-</html>
